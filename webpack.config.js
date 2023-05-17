2817d72ed1f08c36df943e0edbec8c638ed211ba
hyungjun <gudwnd1243@gmail.com>
2023-05-17 19:40:54 +0900

    modify

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     context: __dirname,
     entry: {
-        app: './src/ptzero/wms/index.js'
+        app: './src/hjkim/3dTiles/index.js'
     },
     output: {
         filename: 'app.js',
